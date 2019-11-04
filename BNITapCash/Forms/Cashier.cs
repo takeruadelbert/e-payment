1997f43d0432b20641a7fbec900362b00741ec0d
Lukas Fam <takeruadelbert@gmail.com>
2019-11-04 15:13:19 +0700

    fix bug live camera.

--- BNITapCash/Forms/Cashier.cs
+++ BNITapCash/Forms/Cashier.cs
@@ -43,7 +43,7 @@ namespace BNITapCash
             this.home = home;
             Initialize();
             this.webcamImage = webcam;
-            InitializeWebcam();
+            //InitializeWebcam();
 
         }
 
@@ -55,6 +55,8 @@ namespace BNITapCash
             try
             {
                 stream = new JPEGStream(liveCameraURL);
+                stream.Login = Properties.Settings.Default.LiveCameraUsername;
+                stream.Password = Properties.Settings.Default.LiveCameraPassword;
                 stream.NewFrame += stream_NewFrame;
                 stream.Start();
             }
