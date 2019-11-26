dcd298be8a3a11d0eccfed122cd2984228bdd299
Lukas Fam <takeruadelbert@gmail.com>
2019-11-26 17:04:04 +0700

    update application version.

--- BNITapCash/Properties/AssemblyInfo.cs
+++ BNITapCash/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BNITapCash")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("e-payment application which is integrating with local banks.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("PT. Surya Teknologi Nasional")]
 [assembly: AssemblyProduct("BNITapCash")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("T.K.")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
