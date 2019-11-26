2189e7964443160bc3474cfd8ccf99b123dd3002
Lukas Fam <takeruadelbert@gmail.com>
2019-11-26 11:09:02 +0700

    Free pass (#8)
    * update cashier reformat menu and add menu free pass
    
    * create form free pass
    
    * update csproj

---
+++ BNITapCash/Forms/FreePass.cs
@@ -0,0 +1,65 @@
+﻿using AForge.Video;
+using BNITapCash.API;
+using BNITapCash.API.request;
+using BNITapCash.API.response;
+using BNITapCash.Bank.BNI;
+using BNITapCash.Bank.DataModel;
+using BNITapCash.Card.Mifare;
+using BNITapCash.ConstantVariable;
+using BNITapCash.DB;
+using BNITapCash.Forms;
+using BNITapCash.Helper;
+using BNITapCash.Miscellaneous.Webcam;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BNITapCash.Forms
+{
+    public partial class FreePass : Form
+    {
+        public FreePass()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(Constant.CONFIRMATION_MESSAGE_BEFORE_EXIT, "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                Dispose();
+                System.Environment.Exit(1);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBackToCashier_Click(object sender, EventArgs e)
+        {
+            StopLiveCamera();
+            database.DisposeDatabaseConnection();
+            Cashier cashier = new Cashier(home);
+            cashier.Show();
+            Dispose();
+            GC.Collect();
+        }
+    }
+}
